887b5279d5e3a087d777a8b20476027c771426eb
Stephanie Dursma <stephaniedursma@Stephanies-MacBook-Pro.local>
2018-02-24 17:20:12 -0700

    completed logic starting style

---
+++ assets/javascript/game.js
@@ -0,0 +1,70 @@
+// The random number shown at the start of the game should be between 19 - 120.
+// Each crystal should have a random hidden value between 1 - 12.
+$(document).ready(function(){
+    var goal = Math.floor(Math.random() * (19 - 120)) + 120;
+    var pup1 = Math.floor(Math.random() * (1 - 12) + 12);
+    var pup2 = Math.floor(Math.random() * (1 - 12) + 12);
+    var pup3 = Math.floor(Math.random() * (1 - 12) + 12);
+    var pup4 = Math.floor(Math.random() * (1 - 12) + 12);
+    var total = 0;
+    var win = 0;
+    var losses = 0;
+
+    $('#goal').text('Your Goal Number Is: ' + goal);
+    $('#score').text('Your Current Total Is: ' + total);
+
+    $('#pic1').on('click', function() {
+        console.log(pup1);
+        total = total + pup1
+        $('#score').text('Your Current Total Is: ' + total);
+        checkWinLoss();
+    });
+        
+    $('#pic2').on('click', function() {
+        console.log(pup2);
+        total = total + pup2
+        $('#score').text('Your Current Total Is: ' + total);
+        checkWinLoss();
+    });
+
+    $('#pic3').on('click', function() {
+        console.log(pup3);
+        total = total + pup3
+        $('#score').text('Your Current Total Is: ' + total);
+        checkWinLoss();
+    });
+
+    $('#pic4').on('click', function() {
+        console.log(pup4);
+        total = total + pup4
+        $('#score').text('Your Current Total Is: ' + total);
+        checkWinLoss();
+    });
+
+    function checkWinLoss() {
+        if (goal === total) {
+            win++;
+            alert ("You win!");
+            reset();
+        } else if (total > goal) {
+            losses++;
+            alert ("Sorry You Lose!")
+            reset();
+        }
+    }
+
+    function reset(){
+        goal = Math.floor(Math.random() * (19 - 120)) + 120;
+        pup1 = Math.floor(Math.random() * (1 - 12) + 12);
+        pup2 = Math.floor(Math.random() * (1 - 12) + 12);
+        pup3 = Math.floor(Math.random() * (1 - 12) + 12);
+        pup4 = Math.floor(Math.random() * (1 - 12) + 12);
+        total = 0;
+        $('#goal').text('Your Goal Number Is: ' + goal);
+        $('#score').text('Your Current Total Is: ' + total);
+    }
+});
+
+
+
+
